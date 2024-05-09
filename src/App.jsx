c6320b07f6eaad12b8b695b9b72446fcd1182949
gleidsonteixeira <gleidsondeveloper@gmail.com>
2024-05-09 12:03:10 -0300

    inital

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+
+import "primereact/resources/themes/lara-light-green/theme.css";
+import "primeicons/primeicons.css";
+import "primeflex/primeflex.css";
+
+import { Button } from "primereact/button";
+
+const App = () => {
+  return (
+    <>
+    <Button label={"teste"} />
+    </>
+  );
+}
+ 
+export default App;
