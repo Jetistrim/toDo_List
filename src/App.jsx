d018d7f3965f4565ae2287356e596fac89bf9952
gleidsonteixeira <gleidsondeveloper@gmail.com>
2024-05-14 12:06:54 -0300

    login

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,12 @@
 import "primereact/resources/themes/lara-light-green/theme.css";
 import "primeicons/primeicons.css";
 import "primeflex/primeflex.css";
-
-import { Button } from "primereact/button";
+import Paths from "./routes/Paths";
 
 const App = () => {
   return (
     <>
-    <Button label={"teste"} />
+      <Paths />
     </>
   );
 }
